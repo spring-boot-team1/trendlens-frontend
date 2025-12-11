c58f46262021fba7112f64e8413c2879af0a835e
ppjh94 <wleer157@gmail.com>
2025-12-11 16:24:36 +0900

    feat: 결제 페이지 구현 및 Front 단 결제 테스트 성공

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/inc/header";
-import Example from "./pages/example";
 import Index from "./pages";
+import PaymentCheckout from "./pages/PaymentCheckout";
 
 function App() {
   return (
     <>
       <Header />
-
       <Routes>
-        <Route path="/example" element={<Example />} />
         <Route path="/" element={<Index />} />
+
+        {/* ✅ 결제 페이지 라우트 추가 */}
+        <Route path="/payment/checkout" element={<PaymentCheckout />} />
       </Routes>
     </>
   );
