cc2146d742ab73c326bd14a8622890093ad4d8e7
Sunmin <sunmin323233@gmail.com>
2025-12-11 12:49:28 +0900

    fix: content-type을 xxx-w-form-urlencoded로 변경

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,11 +1,29 @@
 import axiosInstance from "./axiosInstance";
 
-export const login = async(email: string, password:string) => {
+export const login = async(email: string, password: string) => {
+    const params = new URLSearchParams();
+    params.append("username", email);
+    params.append("password", password);
+
     const res = await axiosInstance.post(
         "/login",
-        {email, password},
-        {skipAuth: true}
-    );
+        params, {
+        headers: {
+            "Content-Type" : "application/x-www-form-urlencoded",
+        },
+        skipAuth:true,
+    });
+
+    // const res = await axiosInstance.post(
+    //     "/login", 
+    //     params,
+    //     {
+    //         headers: {
+
+    //         }, 
+    //     },
+    //     {skipAuth: true}
+    // );
     //서버에서 내려준 값에 맞게 수정
     const { accessToken } = res.data;
     
