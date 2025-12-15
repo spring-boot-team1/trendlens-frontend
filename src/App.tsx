b383379633be80039e3b6806df1881c665183115
Sunmin <145127753+hikigirl@users.noreply.github.com>
2025-12-16 04:33:43 +0900

    fix: 라우팅 경로 중복 수정, header 중복 import 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "@/components/inc/header"; 
+import Example from "./pages/example";
+import Index from "./pages";
+
 import RankingPage from "./pages/RankingPage";
 import InsightPage from "./pages/InsightPage";
 import SearchPage from "./pages/SearchPage";
-import Header from "./components/inc/header";
-import Example from "./pages/example";
-import Index from "./pages";
 import BodyAnalyze from "./pages/bodyanalyze";
 import Login from "./pages/login";
 import Register from "./pages/register";
@@ -14,29 +14,22 @@ import Mypage from "./pages/mypage";
 function App() {
   return (
     <div className="min-h-screen bg-slate-50 text-slate-900">
-      
       <Header />
 
-      <main className="pt-15"> 
+      <main className="pt-15">
         <Routes>
-          {/* 메인 페이지 */}
           <Route path="/" element={<RankingPage />} />
-          {/* 검색 페이지*/}
           <Route path="/search" element={<SearchPage />} />
-          {/* 상세 페이지 */}
           <Route path="/insight" element={<InsightPage />} />
+          <Route path="/example" element={<Example />} />
+          <Route path="/index" element={<Index />} />
+          <Route path="/bodyanalyze" element={<BodyAnalyze />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/mypage" element={<Mypage />} />
         </Routes>
       </main>
     </div>
-      <Routes>
-        <Route path="/example" element={<Example />} />
-        <Route path="/" element={<Index />} />
-        <Route path="/bodyanalyze" element={<BodyAnalyze />} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/mypage" element={<Mypage />} />
-      </Routes>
-    </>
   );
 }
 
